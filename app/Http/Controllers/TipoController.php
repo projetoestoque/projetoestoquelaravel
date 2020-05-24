4e2f82de62a0133de3763895ef49d4c8c5e64e2c
Nidhoggui <guilhermemelquiades42@gmail.com>
2020-05-23 21:21:48 -0300

    Função deletar tipo

--- app/Http/Controllers/TipoController.php
+++ app/Http/Controllers/TipoController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class TipoController extends Controller
 {
-    //
+  public function deletarTipo()
+{
+      $tipo_id = $_GET['id'];
+      $tipo = Tipo::find($tipo_id);
+      $tipo->delete();
+      return redirect()->back()->with('status', 'Tipo deletado com sucesso!');
+  }
 }
