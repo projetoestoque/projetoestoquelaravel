97678f11abcdafacd233371d0e0aa1985984e00f
Nidhoggui <guilhermemelquiades42@gmail.com>
2020-05-23 21:13:09 -0300

    Função deletar estoque

--- app/Http/Controllers/EstoqueController.php
+++ app/Http/Controllers/EstoqueController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Doador;
+use DB;
 
 class EstoqueController extends Controller
 {
-    //
+  public function deletarEstoque()
+{
+      $estoque_id = $_GET['id'];
+      $estoque = Estoque::find($estoque_id);
+      $estoque->delete();
+      return redirect()->back()->with('status', 'Estoque deletado com sucesso!');
+  }
 }
