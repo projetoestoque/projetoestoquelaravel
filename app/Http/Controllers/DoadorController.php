c2f91897398926e5fc6e8289338d2b69eb122809
Nidhoggui <guilhermemelquiades42@gmail.com>
2020-05-23 21:08:56 -0300

    Função deletar doador

--- app/Http/Controllers/DoadorController.php
+++ app/Http/Controllers/DoadorController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Doador;
+use DB;
+
 
 class DoadorController extends Controller
 {
-    //
+  public function deletarDoador()
+{
+      $doador_id = $_GET['id'];
+      $doador = Doador::find($doador_id);
+      $doador->delete();
+      return redirect()->back()->with('status', 'Doador deletado com sucesso!');
+  }
 }
