0f3de6c4464a0fbb45082b6dd8c7e8c114937ada
Nidhoggui <guilhermemelquiades42@gmail.com>
2020-05-23 21:16:36 -0300

    Função deletar marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Marca;
+use DB;
 
 class MarcaController extends Controller
 {
-    //
+  public function deletarMarca()
+{
+      $marca_id = $_GET['id'];
+      $marca = Marca::find($marca_id);
+      $marca->delete();
+      return redirect()->back()->with('status', 'Marca deletada com sucesso!');
+  }
 }
