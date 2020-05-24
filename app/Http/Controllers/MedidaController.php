1f021ced815e7375bf042b9c6d0818f379f6012f
Nidhoggui <guilhermemelquiades42@gmail.com>
2020-05-23 21:19:20 -0300

    Função deletar medida

--- app/Http/Controllers/MedidaController.php
+++ app/Http/Controllers/MedidaController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class MedidaController extends Controller
 {
-    //
+  public function deletarMedida()
+{
+      $medida_id = $_GET['id'];
+      $medida = Medida::find($medida_id);
+      $medida->delete();
+      return redirect()->back()->with('status', 'Medida deletada com sucesso!');
+  }
 }
