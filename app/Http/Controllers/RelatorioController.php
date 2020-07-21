408bdd04e85cd524d12e64a4cc97595436000d5a
alephyv <alephyvrossi@gmail.com>
2020-07-21 01:45:22 -0300

    dando inicio no relatorio controller

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -9,4 +9,73 @@ class RelatorioController extends Controller
     public function index(){
         return view('relatorio');
     }
+
+    public function gerarRelatorio(Request $req) {
+        date_default_timezone_set('America/Sao_Paulo');
+
+        switch($req->get('tipo')) {
+            case 'geral':
+                switch($req->get('periodo')) {
+                    case 'dia':
+                        $hoje = date('Y-m-d');
+                        $relatorios = DB::table('relatorios')->where('data', $hoje)->where('tipo','geral')->get();
+                        foreach($relatorio as $relatorios) {
+                            $relatorio_saida += $relatorio->relatorio;
+                        }
+                    break;
+                    case 'semana':
+
+                    break;
+                    case 'mes':
+
+                    break;
+                    case 'ano':
+
+                    break;
+                }
+            break;
+            case 'entrada':
+                case 'dia':
+
+                break;
+                case 'semana':
+
+                break;
+                case 'mes':
+
+                break;
+                case 'ano':
+
+                break;
+            break;
+            case 'vencimento':
+                case 'dia':
+
+                break;
+                case 'semana':
+
+                break;
+                case 'mes':
+
+                break;
+                case 'ano':
+
+                break;
+            break;
+            case 'baixa':
+                case 'dia':
+
+                break;
+                case 'semana':
+
+                break;
+                case 'mes':
+
+                break;
+                case 'ano':
+
+                break;
+            break;
+        }
+    }
 }
--- database/migrations/2020_07_19_203605_create_relatorios_table.php
+++ database/migrations/2020_07_19_203605_create_relatorios_table.php
@@ -15,13 +15,15 @@ class CreateRelatoriosTable extends Migration
     {
         Schema::create('relatorios', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('Id_doador')-> nullable();
-            $table->string('tipo')->enum();
+            $table->unsignedBigInteger('Id_doador')->nullable();
+            $table->unsignedBigInteger('Id_produto')->nullable();
+            $table->enum('tipo', ['entrada', 'geral', 'baixa', 'vencimento']);
             $table->string('relatorio');
             $table->string('data');
             $table->timestamps();
 
             $table->foreign('Id_doador')->references('id')->on('doadors');
+            $table->foreign('Id_produto')->references('id')->on('produtos');
             $table->softDeletes();
         });
     }
